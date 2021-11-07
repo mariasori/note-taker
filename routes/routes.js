18fefc4754cf2d9b344a063519b49af78d5bc95a
Maria Sori <mariasori.school@gmail.com>
2021-11-07 18:52:21 -0500

    functioning delete button

--- routes/routes.js
+++ routes/routes.js
@@ -1,44 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 const notes = require('../db/db.json')
-
-module.exports = app => {    
     
+module.exports = app => {
+    
+    var noteId = 0;
+
     //API routes
     app.get('/api/notes', function (req, res) {
         return res.json(notes);
     })
-    
+
     app.post('/api/notes', function (req, res) {
-        noteId = notes.length;
+        
+        noteId = noteId + 1;
+
         var newNote = { title: req.body.title, text: req.body.text, id: noteId}
         notes.push(newNote);
-        updateNotes();
-        res.json(notes);
-        return console.log('Added new note: ' + newNote.title)
-    });
-    
-    app.delete('/api/notes/:id', function (req, res) {
-        notes.splice(req.params.id);
-        updateNotes();
-        return console.log('Deleted note with id ' + req.params.id);
-    });
-    
-    function updateNotes() {
+
         fs.writeFile('./db/db.json', JSON.stringify(notes), function(err) {
             if (err) {
                 return console.log(err);
             }
-            console.log('Notes updated!')
         });
-    }
-    
+
+        res.json(notes);
+        return console.log('Added new note: ' + newNote.title)
+    });
+
+    app.delete('/api/notes/:id', function (req, res) {
+        const { id } = req.params;
+
+        const notesIndex = notes.findIndex(p => p.id == id);
+
+        notes.splice(notesIndex, 1);
+
+        res.json(notes);
+        return console.log('Deleted note with id ' + req.params.id);
+    });
+
     //HTML routes
     app.get('/notes', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-    
+
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 }
+
