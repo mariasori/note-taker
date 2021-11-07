76db10746675beb3f9120ff801bbef262ffd1f7f
Maria Sori <mariasori.school@gmail.com>
2021-11-07 12:42:36 -0500

    new notes now populating

--- routes/routes.js
+++ routes/routes.js
@@ -4,28 +4,28 @@ const notes = require('../db/db.json')
 
 module.exports = app => {    
     
-    currentID = notes.length;
-
     //API routes
     app.get('/api/notes', function (req, res) {
         return res.json(notes);
     })
     
     app.post('/api/notes', function (req, res) {
-        var newNote = req.body;
+        noteId = notes.length;
+        var newNote = { title: req.body.title, text: req.body.text, id: noteId}
         notes.push(newNote);
         updateNotes();
+        res.json(notes);
         return console.log('Added new note: ' + newNote.title)
     });
     
     app.delete('/api/notes/:id', function (req, res) {
-        notes.splice(req.params.id, 1);
+        notes.splice(req.params.id);
         updateNotes();
         return console.log('Deleted note with id ' + req.params.id);
     });
     
     function updateNotes() {
-        fs.writeFile('db/db.json', JSON.stringify(notes), function(err) {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), function(err) {
             if (err) {
                 return console.log(err);
             }
